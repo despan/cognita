116c4b6e201bd5503419602b37f22649fedb23b0
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-12-13 11:26:08 +0400

    Refine dev. env.

--- containers/web-client/src/store/index.js
+++ containers/web-client/src/store/index.js
@@ -1,12 +1,19 @@
 import {
   createStore,
-  applyMiddleware
+  applyMiddleware,
+  compose
 } from 'redux'
 
 import thunk from 'redux-thunk'
 
-import reducer from './reducers'
+import reducer from './reducer'
 
 const middleware = applyMiddleware(thunk)
 
-export default createStore(reducer, middleware)
+export default createStore(
+  reducer,
+  compose(
+    middleware,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
